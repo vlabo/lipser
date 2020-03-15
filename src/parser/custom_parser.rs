ddcbc71301fdb044a6823f912b4d1eecd8c22fac
vlabo <vladdocan@gmail.com>
2020-03-15 15:59:35 +0200

    Spliting global functions in custom parser

--- src/parser/custom_parser.rs
+++ src/parser/custom_parser.rs
@@ -1,21 +1,44 @@
 use super::LispValue;
 
-fn parse_function(code: &[char]){
-    let rest: String = code.into_iter().collect();
-    println!("{}", rest);
+//fn read_fucntion(code: &[char]) {
+//}
+
+fn split_functions(code: &[char]) -> Vec<(usize, usize)> {
+    let mut open_count = 0;
+    let mut current_function = (0, 0);
+    let mut functions = Vec::new();
+    for i in 0..code.len() {
+        match code[i] {
+            '(' => {
+                open_count += 1;
+                if open_count == 1 {
+                    current_function.0 = i;
+                }
+            },
+            ')' => {
+                open_count -= 1;
+                if open_count == 0 {
+                    current_function.1 = i + 1;
+                    functions.push(current_function);
+                }
+            },
+            _ => {},
+        };
+    }
+    return functions;
 }
 
-
 pub fn parse(code: &str) -> Option<Vec<LispValue>> {
-    let chars: Vec<char> = code.chars().collect();
-    for i in 0..chars.len() {
-        match chars[i] {
-            '(' => parse_function(&chars[i..]),// read function
-            _ => (),// do nothing
-        }
-
+    let chars : Vec<char> = code.chars().collect();
+    let functions_position = split_functions(&chars[..]);
+    
+    for f in functions_position {
+        println!("{}", &chars[f.0..f.1].into_iter().collect::<String>());
+        println!("----------------");
     }
 
-    let values = vec![LispValue::Function(vec![LispValue::Name("print".to_string()), LispValue::String("Hello custom parser".to_string())])];
+    let values = vec![
+        LispValue::Function(vec![LispValue::Name("print".to_string()), LispValue::String("Hello custom parser".to_string())])
+    ];
     Some(values) 
 }
